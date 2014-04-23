40de47755465442622b5508909f9b53633335130
Raspberry <nazmus@outlook.com>
2014-04-23 14:50:35 -0500

    Updated fully working code for the core functionality. Email still needs work

--- doorbell_core.py
+++ doorbell_core.py
@@ -57,47 +57,4 @@ while True:
                 dig_2 = 0
                 dig_3 = 0
                 dig_4 = 0
-                display.writeDigit(4, 0, False)
-
-    
-import smtplib
-import time
-import string
-
-    configFile = open('/home/pi/doorbelldoorbell config.txt','r')
-    lineCount = 0
-	for line in configFile:
-	lineCount = lineCount + 1
-
-	if lineCount == 1:
-	    FROM = line
-	elif lineCount == 2:
-	    TO = line
-	elif lineCount == 3:
-              USERNAME = line
-        elif lineCount == 4:
-            PASSWORD = line
-        else:
-            print "ERROR: line count of doorbell_config.txt exceeds standard bounds. Make sure the doorbell_config.txt is formatted exactly as specefied. Please see \"How to setup doorbell_config.txt\" for instructions. Line count = " + lineCount + "."
-        lineCount = 0
-        configFile.close()
-
-        SUBJECT = "Doorbell Monitor Notification"
-        text = "Someone rang your doorbell at " + time.strftime('%Y-%m-%d %H:%M:%S') + "."
-        #Leave the following BODY block intact. Only change the values above.
-        BODY = string.join((
-        "From: %s" % FROM,
-        "To: %s" % TO,
-        "Subject: %s" % SUBJECT ,
-        "",
-        text
-        ), "\r\n")
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(USERNAME, PASSWORD)
-        server.sendmail(FROM, [TO], BODY)
-        server.close()
-        #End of code snippet
-
+                display.writeDigit(4, 0, False)        
